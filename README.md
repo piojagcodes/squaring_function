5a2e61b1d84fb7d12ad21ee079f60e356e8adeb3
piojagcodes <piojag@gmail.com>
2022-04-03 21:24:23 +0200

    Final commit before deadline.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # squaring_function
 Basic C++ project
+
+
+Application is compiled on Fedora distro with clang++ compiler
+
+for package download type in command:
+sudo dnf install clang++
+
+
+After creating file with .cpp extension just compile it by using clang++ [name_of_file] in terminal
+
+
+App is basically mine proposition for expanding fences. For initiation of mine idea i used percentages which could fit into formula a + a + b
+Expansion to bigger areas on other hand gave me thought about abbreviated multiplication formulas.
+
--- fermville.cpp
+++ fermville.cpp
@@ -9,7 +9,7 @@ void input_for_area_measurement();
 
 int main() {
 	launcher();
-	
+	input_for_area_measurement();
 
 }
 
@@ -32,6 +32,41 @@ void launcher() {
 	}
 
 	}
-
-
 }
+
+void input_for_area_measurement() {
+	int area_number;
+	int measured_quarter_from_area;
+	
+	const int stored_quarter = 25;
+	const int fully_stored_percentage = 100;
+	const int stored_half = 50;
+	const int doubled = 2;
+
+	int area_number_substracted_by_quarter;
+	int area_number_substracted_by_half;
+	int proposition_of_area_expansion;
+
+
+	cout<<"Write down number for area measurement"<<endl;
+	cin>>area_number;
+	measured_quarter_from_area = area_number * stored_quarter / fully_stored_percentage;
+	
+	area_number_substracted_by_quarter = area_number - measured_quarter_from_area;
+	area_number_substracted_by_half = area_number_substracted_by_quarter * stored_half / fully_stored_percentage;
+
+	cout<<"Measured from area number sides are equal to: "<<area_number_substracted_by_half<<","<<measured_quarter_from_area<<endl;
+
+
+	int raising_to_the_power_of_second_half = pow(area_number_substracted_by_half, 2);
+	int raising_to_the_power_of_second_quarter = pow(area_number_substracted_by_quarter, 2);
+	int double_up_area_number = doubled * measured_quarter_from_area * area_number_substracted_by_half;
+
+	cout<<"Sides for larger area"<<raising_to_the_power_of_second_half<<" "<<double_up_area_number<<" "<<raising_to_the_power_of_second_quarter<<endl;
+
+	proposition_of_area_expansion = raising_to_the_power_of_second_half + double_up_area_number + raising_to_the_power_of_second_quarter;
+
+	cout<<"Expanded area is equal to:"<<proposition_of_area_expansion<<endl;
+}
+
+
