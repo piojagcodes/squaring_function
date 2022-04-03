0eb43cefa951a20a3e0b3b9bd5402c4feba56e19
piojagcodes <piojag@gmail.com>
2022-04-03 19:42:59 +0200

    Initialising launcher method from program

--- fermville.cpp
+++ fermville.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
+void launcher();
+void input_for_area_measurement();
 
 int main() {
+	launcher();
+	
 
-	cout<<"Hello Github"<<endl;
+}
+
+void launcher() {
+	string input;
+	string launch_word = "Run";
+
+	while(input!= launch_word)
+	{
+	cout<<" Write Run for launch" <<endl;
+	cin>>input;
+
+	if (input == launch_word)
+	{
+	cout<<"You have initialised Fermville program"<<endl;
+	}
+
+	else if(input!= launch_word){
+	cout<<"Please try again"<<endl;
+	}
+
+	}
+
 
 }
